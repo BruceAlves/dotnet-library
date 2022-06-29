8f6c16391f6cbbfdbbaaba7037d733adf831276a
MarcioEudes <marcioeudespf@gmail.com>
2022-06-29 06:04:00 -0300

    Implementação tela de menu

---
+++ dotnet_library/Menu.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace dotnet_library
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.btnCadastroUsuario = new System.Windows.Forms.Button();
+            this.btnCadastroCliente = new System.Windows.Forms.Button();
+            this.btnCadastrarLivro = new System.Windows.Forms.Button();
+            this.btnEmprestimos = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCadastroUsuario
+            // 
+            this.btnCadastroUsuario.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCadastroUsuario.Location = new System.Drawing.Point(12, 9);
+            this.btnCadastroUsuario.Name = "btnCadastroUsuario";
+            this.btnCadastroUsuario.Size = new System.Drawing.Size(113, 23);
+            this.btnCadastroUsuario.TabIndex = 0;
+            this.btnCadastroUsuario.Text = "Cadastro Usuário";
+            this.btnCadastroUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastroCliente
+            // 
+            this.btnCadastroCliente.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnCadastroCliente.Location = new System.Drawing.Point(12, 38);
+            this.btnCadastroCliente.Name = "btnCadastroCliente";
+            this.btnCadastroCliente.Size = new System.Drawing.Size(113, 23);
+            this.btnCadastroCliente.TabIndex = 1;
+            this.btnCadastroCliente.Text = "Cadastro Cliente";
+            this.btnCadastroCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnCadastrarLivro
+            // 
+            this.btnCadastrarLivro.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnCadastrarLivro.Location = new System.Drawing.Point(12, 67);
+            this.btnCadastrarLivro.Name = "btnCadastrarLivro";
+            this.btnCadastrarLivro.Size = new System.Drawing.Size(113, 23);
+            this.btnCadastrarLivro.TabIndex = 2;
+            this.btnCadastrarLivro.Text = "Cadastrar Livro";
+            this.btnCadastrarLivro.UseVisualStyleBackColor = false;
+            // 
+            // btnEmprestimos
+            // 
+            this.btnEmprestimos.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnEmprestimos.Location = new System.Drawing.Point(12, 96);
+            this.btnEmprestimos.Name = "btnEmprestimos";
+            this.btnEmprestimos.Size = new System.Drawing.Size(113, 23);
+            this.btnEmprestimos.TabIndex = 3;
+            this.btnEmprestimos.Text = "Emprestimos";
+            this.btnEmprestimos.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(152, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 435);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnCadastroUsuario);
+            this.panel1.Controls.Add(this.btnCadastroCliente);
+            this.panel1.Controls.Add(this.btnEmprestimos);
+            this.panel1.Controls.Add(this.btnCadastrarLivro);
+            this.panel1.Location = new System.Drawing.Point(4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(142, 435);
+            this.panel1.TabIndex = 5;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.Text = "Dotnet Library";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCadastroUsuario;
+        private System.Windows.Forms.Button btnCadastroCliente;
+        private System.Windows.Forms.Button btnCadastrarLivro;
+        private System.Windows.Forms.Button btnEmprestimos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
